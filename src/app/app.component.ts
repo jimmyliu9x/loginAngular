3dd33506511091fc69ba5ee314c629fa03a2b8be
caonan <info@caonan.com>
2022-05-05 16:53:59 +0800

    fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,11 +40,11 @@ export class AppComponent implements OnInit {
     this.isLoggedIn = !!this.tokenStorageService.getToken();
     if (this.isLoggedIn) {
       const user = this.tokenStorageService.getUser();
-      this.roles = user.roles;
+/*       this.roles = user.roles;
       this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
-      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR'); */
       this.username = user.username;
-      window.location.href = '/';
+      //window.location.href = '/';
     }else{
       
       //window.location.href = '/';
